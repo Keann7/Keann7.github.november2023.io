63d7092b229627fa71ffbce247779b8322f3762e
Keann7 <145884165+Keann7@users.noreply.github.com>
2023-11-19 17:39:09 +0700

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,11 @@
+const password = 'sepeda'; // Ganti dengan kata sandi yang diinginkan
+
+function checkPassword() {
+  const passwordInput = document.getElementById('passwordInput').value;
+
+  if (passwordInput === password) {
+    window.location.href = 'foto.html'; // Ganti dengan halaman yang ingin diakses setelah kata sandi benar
+  } else {
+    document.getElementById('message').innerText = 'Kata Sandi Salah!';
+  }
+}
